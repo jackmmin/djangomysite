c0d52868c215f75539bbfbe12e43dfa14e318006
jackmmin <jongmin0409@naver.com>
2019-11-19 18:48:01 +0900

    init

--- polls/models.py
+++ polls/models.py
@@ -44,16 +44,37 @@ class Etc_data(models.Model):
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
-class Feedback(models.Model):
-    feedback_text = models.CharField(max_length=200)
+class Feedback_Question(models.Model):
+    feedback_q = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.etc_text
+        return self.feedback_q
 
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
+
+class Feedback_Choice(models.Model):
+    feedback_q = models.ForeignKey(Feedback_Question, on_delete=models.CASCADE)
+    feedback_c = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.feedback_c
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
+class Fb_Choice_Data(models.Model):
+    feedback_c = models.ForeignKey(Feedback_Choice, on_delete=models.CASCADE, default="", editable=False)
+    fb_etc_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.etc_bad_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
--- polls/views.py
+++ polls/views.py
@@ -2,17 +2,34 @@ from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
-from .models import Question, Choice, Etc_data, Feedback
+from .models import Question, Choice, Etc_data, Feedback_Choice, Fb_Choice_Data, Feedback_Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+    fb_c_list = Feedback_Choice.objects.all()
+    feedback_question = Feedback_Question.objects.all()
+    context = {'latest_question_list': latest_question_list, 'fb_c_list': fb_c_list}
 
-    if request.POST.get('fb', False):
-        fb_text = Feedback(feedback_text=request.POST.get('fb', False))
-        fb_text.save()
+    # if request.POST.get('fb', False):
+    #     fb_text = Feedback(feedback_text=request.POST.get('fb', False))
+    #     fb_text.save()
 
+    # selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    try:
+        if request.POST.get('fb_c', False):
+            q = Feedback_Question.objects.get(pk=1)
+            fb_choice = q.feedback_choice_set.get(pk=request.POST['fb_c'])
+            fb_choice.votes += 1
+            fb_choice.save()
+
+        if request.POST.get('fb_choice_text'):
+            pk = 2
+            fb_text = Fb_Choice_Data(fb_etc_text=request.POST.get('fb_choice_text'), feedback_c_id=pk)
+            fb_text.save()
+
+    except():
+        return render(request, 'polls/index.html')
     return render(request, 'polls/index.html', context)
 
 
